e3c255f44f696c2df95681beb1c42e8dcaad009b
Nirupa03 <nirupa0312@gmail.com>
2019-03-03 16:21:29 +0000

    My first Commit

---
+++ src/love/programming/MyHomeWorkMain.java
@@ -0,0 +1,9 @@
+package love.programming;
+
+public class MyHomeWorkMain {
+    public static void main(String[] args) {
+        MyHomeWorkClass1 MyHomeWork1 = new MyHomeWorkClass1();
+        MyHomeWorkClass2 MyhomeWork2 = new MyHomeWorkClass2();
+
+    }
+}
